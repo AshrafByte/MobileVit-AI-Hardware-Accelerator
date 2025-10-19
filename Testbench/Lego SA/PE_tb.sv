d8700d4aec20ab50997d6ff8d48848248135f965
Ammar Wahidi <aammarahmed123@gmail.com>
2025-10-19 23:03:34 +0300

    Add Lego SA_v1

--- Testbench/Lego SA/PE_tb.sv
+++ Testbench/Lego SA/PE_tb.sv
@@ -5,11 +5,15 @@ logic                    rst_n       ;
 logic                    valid_in    ;
 logic [DATA_W-1:0]       in_act      ;       // input activation from left
 logic [DATA_W_OUT-1:0]   in_psum     ;       // partial sum from top
-logic [DATA_W-1:0]       weight_load ;       // new weight (when load_w = 1)
+logic [DATA_W-1:0]       w_in_down   ;  
+logic [DATA_W-1:0]       w_in_left   ; 
 logic                    load_w      ;       // load weight enable
+logic                    transpose_en;
 
 logic [DATA_W-1:0]       out_act     ;       // propagate activation right
 logic [DATA_W_OUT-1:0]   out_psum    ;       // propagate partial sum down
+logic [DATA_W-1:0]       w_out_up       ;
+logic [DATA_W-1:0]       w_out_right     ;
 logic                    valid_out   ;
 
 PE #(.DATA_W(DATA_W),.DATA_W_OUT(DATA_W_OUT)) PE_mac (
@@ -18,10 +22,14 @@ PE #(.DATA_W(DATA_W),.DATA_W_OUT(DATA_W_OUT)) PE_mac (
 .valid_in(valid_in),
 .in_act(in_act),
 .in_psum(in_psum),
-.weight_load(weight_load),
+.w_in_down(w_in_down),
+.w_in_left(w_in_left),
 .load_w(load_w),
+.transpose_en(transpose_en),
 .out_act(out_act),
 .out_psum(out_psum),
+.w_out_up(w_out_up),
+.w_out_right(w_out_right),
 .valid_out(valid_out)
 );
 
@@ -37,21 +45,21 @@ begin
     load_w = 1 ;
     in_act = 0 ;
     in_psum = 0 ;
-    weight_load = 5 ;
+    transpose_en = 0 ;
+    w_in_down = 5 ;
+    w_in_left = 10 ;
     $display("[%0t]Loading Wights",$time);
     #10;
-    valid_in = 1 ;
-    load_w = 0 ;
-    in_act = 9 ;
-    in_psum = 55 ;
-    weight_load = 5 ;
-    $display("[%0t]Computing",$time);
-    $display("valid_in = %0d ,W = %0d ,act_reg = %0d,psum_reg = %0d",valid_in,PE_mac.W_reg,PE_mac.act_reg,PE_mac.psum_reg);
-    $display("out_act = %0d, out_psum = %0d, valid_out = %0d",out_act,out_psum,valid_out);
-    #1;
-    $display("[%0t]Computing",$time);
-    $display("valid_in = %0d ,W = %0d ,act_reg = %0d,psum_reg = %0d",valid_in,PE_mac.W_reg,PE_mac.act_reg,PE_mac.psum_reg);
-    $display("out_act = %0d, out_psum = %0d, valid_out = %0d",out_act,out_psum,valid_out);
+    load_w = 0;
+    in_act = 10 ;
+    in_psum = 40 ;
+    #5;
+    $display("[%0t] Computing ",$time);
+    $display("out_act = %0d , out_psum = %0d , w_out_up = %0d , w_out_right = %0d , valid_out = %0d ",
+    out_act,out_psum,w_out_up,w_out_right,valid_out);
+    $display("W_reg = %0d",PE_mac.W_reg);
+    #5;
+
     $stop;
 end
 endmodule
